97443fe54944005c0a21fe07cd31f83802d5ca55
Tiago <tiagolican@hotmail.com>
2023-05-10 22:39:55 -0300

    Comunicação com backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,28 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Tiago',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Passa informação para componente filho',
-      autoria: 'Tiago',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Comunicação entre componentes Comunicação entre componentes Comunicação entre componentes Comunicação entre componentes Comunicação entre componentes Comunicação entre componentes Comunicação entre componentes Comunicação entre componentes',
-      autoria: 'José',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Passa informação para componente filho',
-      autoria: 'João Vitor',
-      modelo: 'modelo3'
-    }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
